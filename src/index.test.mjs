ea9c3c765684d7835df6a8619964febf1d3ef607
Juha Lindstedt <juha@pakastin.fi>
2018-11-02 15:50:26 +0200

    Add tests

--- src/index.test.mjs
+++ src/index.test.mjs
@@ -1,15 +1,23 @@
 import t from './index.mjs';
 
-import checkReady from './checkready.test.mjs';
-import colors from './colors.test.mjs';
+import plan from './plan.test.mjs';
+import pass from './pass.test.mjs';
+import fail from './fail.test.mjs';
 import equal from './equal.test.mjs';
 import deepEqual from './deepEqual.test.mjs';
+import checkReady from './checkready.test.mjs';
+import colors from './colors.test.mjs';
+import test from './test.test.mjs';
 
 t('Test', (t) => {
-  t('checkReady', checkReady);
-  t('colors', colors);
+  t('plan', plan);
+  t('pass', pass);
+  t('fail', fail);
   t('equal', equal);
   t('deepEqual', deepEqual);
+  t('checkReady', checkReady);
+  t('colors', colors);
+  t('test', test);
 }, () => {
   console.log('Done');
 });
