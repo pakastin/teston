455c632d3285fea6178902307e67cf5ae05deb84
Juha Lindstedt <juha@pakastin.fi>
2018-11-02 15:50:26 +0200

    Add tests

---
+++ src/fail.test.mjs
@@ -0,0 +1,16 @@
+import fail from './fail.mjs';
+
+export default (t) => {
+  t('Fail', (t) => {
+    t.plan(1);
+
+    const obj = {
+      planned: 1,
+      results: []
+    };
+
+    fail(obj, () => {
+      t.deepEqual(obj.results, [false], 'Fail');
+    })('a', 'b');
+  });
+};
